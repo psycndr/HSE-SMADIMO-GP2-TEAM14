fd4e8f0a0987d0a81f6d77c42e0cd4c992e02d3e
IvanVokhmintsev <vokhmintsev.vanya@gmail.com>
2026-04-10 00:37:52 +0300

    Added base url and params

--- scraping.py
+++ scraping.py
@@ -2,3 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
+BASE_URL = 'https://www.tripadvisor.ru/FindRestaurants'
+params = {
+    'geo': '60763',
+    'offset': '0',
+    'establishmentTypes': '10591',
+    'broadened': 'false'
+}
